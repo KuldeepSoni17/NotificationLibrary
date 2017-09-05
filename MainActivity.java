f7261507dcc0f9ab689ea2319718fe1e845e07e4
KuldeepSoni17 <kdpsoni17@gmail.com>
2017-09-05 15:01:57 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -13,7 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Intent i=new Intent(this, NotiService.class);
-        //startService(i);
+        //START THE SERVICE FOR RECEIVING NOTIFICATIONS and DATA.
+        Intent i=new Intent(this, NotiService.class);
+        startService(i);
     }
 }
